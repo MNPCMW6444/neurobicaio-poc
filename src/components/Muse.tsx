f5d0171109478df1da54be5f319dfb0568b6a462
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-26 01:42:12 +0300

    server

--- src/components/Muse.tsx
+++ src/components/Muse.tsx
@@ -14,13 +14,71 @@ const Muse = () => {
         await muse.connect();
         await muse.start();
 
+        const frequencyBands = {} as any;
 
-        muse.eegReadings.pipe(
-            epoch({duration: 256, interval: 100,}) as any,
-            fft({bins: 256}) as any,
-            powerByBand() as any
-        ).subscribe(reading => {
+
+        enum FrequencyBandsPoints {
+            minTheta = 4,
+            minAlphaLow = 8,
+            minAlphaHigh = 10,
+            minBetaLow = 12.5,
+            minBetaMid = 16.5,
+            minBetaHigh = 21,
+            minGamma = 30,
+            max = 60,
+        }
+
+
+        const FrequencyBands = {
+            THETA: {
+                minFrequencyiInHz: FrequencyBandsPoints.minTheta,
+                maxFrequencyiInHz: FrequencyBandsPoints.minAlphaLow,
+            },
+            ALPHA_LOW: {
+                minFrequencyiInHz: FrequencyBandsPoints.minAlphaLow,
+                maxFrequencyiInHz: FrequencyBandsPoints.minAlphaHigh,
+            },
+            ALPHA_HIGH: {
+                minFrequencyiInHz: FrequencyBandsPoints.minAlphaHigh,
+                maxFrequencyiInHz: FrequencyBandsPoints.minBetaLow,
+            },
+            BETA_LOW: {
+                minFrequencyiInHz: FrequencyBandsPoints.minBetaLow,
+                maxFrequencyiInHz: FrequencyBandsPoints.minBetaMid,
+            },
+            BETA_MID: {
+                minFrequencyiInHz: FrequencyBandsPoints.minBetaMid,
+                maxFrequencyiInHz: FrequencyBandsPoints.minBetaHigh,
+            },
+            BETA_HIGH: {
+                minFrequencyiInHz: FrequencyBandsPoints.minBetaHigh,
+                maxFrequencyiInHz: FrequencyBandsPoints.minGamma,
+            },
+            GAMMA: {
+                minFrequencyiInHz: FrequencyBandsPoints.minGamma,
+                maxFrequencyiInHz: FrequencyBandsPoints.max,
+            },
+        };
+
+
+        const freqrange = Object.values(FrequencyBands);
+        const freqnames = Object.keys(FrequencyBands);
+
+
+        freqnames.forEach((freqname: string, index: number) => {
+            frequencyBands[freqname] = [
+                freqrange[index].minFrequencyiInHz,
+                freqrange[index].maxFrequencyiInHz,
+            ];
         });
+
+        frequencyBands &&
+        muse.eegReadings.pipe(
+            epoch({duration: 256, interval: 100}) as any,
+            fft({bins: 256}) as any,
+            powerByBand(frequencyBands) as any
+        ).subscribe(x => console.log(x));
+
 
         /* setMuseState((prevState: any) => ({
              ...prevState,
